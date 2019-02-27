dd9b19bd2f30bd1271181e69ba5b469c99980c12
Patrick Chiang <patrick@yun.to>
2019-02-27 11:19:33 +0800

    manage.py to add DB initialization

--- services/users/manage.py
+++ services/users/manage.py
@@ -3,10 +3,20 @@
 
 from flask.cli import FlaskGroup
 
-from project import app
+from project import app, db  # new
 
 
 cli = FlaskGroup(app)
+
+# new
+# This registers a new command, recreate_db, to the CLI 
+# so that we can run it from the command line, which we'll use shortly to apply the model to the database
+@cli.command('recreate_db')
+def recreate_db():
+    db.drop_all()
+    db.create_all()
+    db.session.commit()
+
 
 
 if __name__ == '__main__':
