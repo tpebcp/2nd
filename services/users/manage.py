7c7192edeb8b2c61d628162431e0ad27326e9afa
Patrick Chiang <patrick@yun.to>
2019-03-05 16:25:46 +0800

    add coverage

--- services/users/manage.py
+++ services/users/manage.py
@@ -1,11 +1,24 @@
 # services/users/manage.py
 
 import unittest
+import coverage
 
 from flask.cli import FlaskGroup
 
 from project import create_app, db  # new
 from project.api.models import User  # new
+
+# coverge here to help
+
+COV = coverage.coverage(
+    branch=True,
+    include='project/*',
+    omit=[
+        'project/tests/*',
+        'project/config.py',
+    ]
+)
+COV.start()
 
 # to read __init__.py under folder project so it can include variable app and db
 
@@ -37,6 +50,21 @@ def seed_db():
     db.session.add(User(username='michael', email="hermanmu@gmail.com"))
     db.session.add(User(username='michaelherman', email="michael@mherman.org"))
     db.session.commit()
+
+@cli.command()
+def cov():
+    """Runs the unit tests with coverage."""
+    tests = unittest.TestLoader().discover('project/tests')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        COV.stop()
+        COV.save()
+        print('Coverage Summary:')
+        COV.report()
+        COV.html_report()
+        COV.erase()
+        return 0
+    return 1
 
 if __name__ == '__main__':
     cli()
